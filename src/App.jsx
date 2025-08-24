b8e15555e9512fe48ef091c891ce9398dc97b7a5
Felipe <jabule3a@gmail.com>
2025-08-24 11:04:08 -0300

    correção ortografica

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
   return (
     <div className="w-screen min-h-screen bg-slate-500 flex justify-center p-6 ">
       <div className="w-[500px] space-y-4">
-        <Title>Gerednciador de Tarefas</Title>
+        <Title>Gerenciador de Tarefas</Title>
 
         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
 
